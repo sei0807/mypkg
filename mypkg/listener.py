285d4793fe121e2719506244f65991baec145b49
Sei <busutobui@gmail.com>
2026-01-18 14:23:05 +0900

    listener.py変更

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -5,20 +5,26 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
-
 class Listener(Node):
     def __init__(self):
         super().__init__('listener')
-        self.pub = self.create_subscription(Int16, 'count', self.cb, 10)
+        self.pub = self.create_publisher(Int16, 'position', 10)
+        self.sub = self.create_subscription(Int16, 'count', self.cb, 10)
         self.pos = 0
 
     def cb(self, msg):
         self.pos += msg.data
-        self.get_logger().info(
-            "Velocity: %d, Current Position: %d" %
-            (msg.data, self.pos))
-
+        msg_out = Int16()
+        msg_out.data = self.pos
+        self.pub.publish(msg_out)
+        self.get_logger().info("Velocity: %d, Current Position: %d" % (msg.data, self.pos))
 
 def main():
     rclpy.init()
     node = Listener()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    node.destroy_node()
+    rclpy.shutdown()
